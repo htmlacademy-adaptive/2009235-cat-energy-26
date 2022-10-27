abaa51045bcd6d0c66c25f5a2c03cd5ba1721afe
PolinaT <timoninapa@gmail.com>
2022-10-27 14:17:30 +0300

    останавливаем работу скрипта если элемент на стр не найден

--- source/js/nav.js
+++ source/js/nav.js
@@ -1,18 +1,22 @@
-const noJS = document.querySelector('.main-nav');
-const main_menu = document.querySelector('.main-nav__wrapper');
-const main_button = document.querySelector('.main-nav__toggle');
+(() => {
+  const noJS = document.querySelector('.main-nav');
+  const main_menu = document.querySelector('.main-nav__wrapper');
+  const main_button = document.querySelector('.main-nav__toggle');
 
-noJS.classList.remove('main-nav--no-js');
+  if (!main_button) return;
 
-main_button.addEventListener('click', () => {
-  main_menu.classList.toggle('main-nav__wrapper--opened');
-  if (main_menu.classList.contains('main-nav__wrapper--opened')) {
-    main_menu.classList.remove('main-nav__wrapper--closed');
-    }
-    else {
-      main_menu.classList.add('main-nav__wrapper--closed');
-    }
+  noJS.classList.remove('main-nav--no-js');
 
-  main_button.classList.toggle('main-nav__toggle--closed');
-  main_button.classList.toggle('main-nav__toggle--opened');
-});
+  main_button.addEventListener('click', () => {
+    main_menu.classList.toggle('main-nav__wrapper--opened');
+    if (main_menu.classList.contains('main-nav__wrapper--opened')) {
+      main_menu.classList.remove('main-nav__wrapper--closed');
+      }
+      else {
+        main_menu.classList.add('main-nav__wrapper--closed');
+      }
+
+    main_button.classList.toggle('main-nav__toggle--closed');
+    main_button.classList.toggle('main-nav__toggle--opened');
+  })
+})();
